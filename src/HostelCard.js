b810492505070936ef13075acc68a17a7ee36564
joshua-pirwoth <joshuapirwoth06@gmail.com>
2023-10-08 15:38:41 +0300

    initial hostel card

---
+++ src/HostelCard.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const HostelCard = () => {
+
+    return ( 
+        <div className="hostel-card">
+            {/* Carousel */}
+            
+            {/* Hostel details */}
+            <div className="hostel-details">
+                <h3>Hostel Name</h3>
+                <p>Hostel Location (e.g. Kikoni Makerere)</p>
+                <p>Available Rooms</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default HostelCard;
