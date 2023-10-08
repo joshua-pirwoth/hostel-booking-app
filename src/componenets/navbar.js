2ebb94e54c1018e14101283deb98e4f494a43a92
MuziraS <stakaudens@gmail.com>
2023-10-08 18:21:12 +0300

    search box

--- src/componenets/navbar.js
+++ src/componenets/navbar.js
@@ -1,51 +1,4 @@
-// import Recat from 'react'
-// import {link} from 'react-router-dom'
-// import "./navbar.css"
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// // import * as Icons from "react-icons/fa";
-// import "./navbar.css";
-// import { navitems } from "./navitems";
-// import button from "./button";
-// import dropdown from "./dropdown";
 
-// function Navbar(){
-//     const [dropdown, setDropdown] = useState(false);
-//     return (
-//         <>
-//         <nav className='navbar'>
-//             <link to="/" className="navbar-logo">
-            
-//             </link>
-//             <ul className="nav-items">
-//           {navitems.map((item) => {
-//             if (item.title === "Services") {
-//               return (
-//                 <li
-//                   key={item.id}
-//                   className={item.cName}
-//                   onMouseEnter={() => setDropdown(true)}
-//                   onMouseLeave={() => setDropdown(false)}
-//                 >
-//                   <Link to={item.path}>{item.title}</Link>
-//                   {dropdown && <dropdown />}
-//                 </li>
-//               );
-//             }
-//             return (
-//               <li key={item.id} className={item.cName}>
-//                 <Link to={item.path}>{item.title}</Link>
-//               </li>
-//             );
-//           })}
-//         </ul>
-//         <button />
-//         </nav>
-//         </>
-//     );
-// }
-
-// export default Navbar;
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
@@ -60,38 +13,25 @@ function Navbar() {
 
   return (
     <>
-      {/* Navigation bar */}
-      <nav className="navbar">
-        {/* Logo link */}
+      {
+      /* Navigation bar */
+      }
+      <nav className="navbar flex ">
+        {
+        /* Logo link */}
+       
         <Link to="/" className="navbar-logo">
           HostelHub
         </Link>
-        {/* Navigation items */}
-        <ul className="nav-items">
-          {navitems.map((item) => {
-            if (item.title === "Services") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdown(true)} // Show dropdown on mouse enter
-                  onMouseLeave={() => setDropdown(false)} // Hide dropdown on mouse leave
-                >
-                  <Link to={item.path}>{item.title}</Link>
-                  {/* Render the Dropdown component conditionally */}
-                  {dropdown && <Dropdown />} {/* Use the Dropdown component */}
-                </li>
-              );
-            }
-            return (
-              <li key={item.id} className={item.cName}>
-                <Link to={item.path}>{item.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-        {/* Render the Button component */}
-        <Button />
+      <div className="flex box-container shadow-md  ">
+        <div>All</div>
+        <div className="search-item"></div>
+        <input type="text" class="search-input" placeholder="Search..."></input>
+      </div>
+      <div className="profile-container flex">
+
+      </div>
+        
       </nav>
     </>
   );
