373733dddbb4192903c7444cd9b4592079a4b89d
MuziraS <stakaudens@gmail.com>
2023-10-16 10:00:48 +0300

    footer function

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,21 @@
 import React from "react";
 import "./style.css";
 
-// function Footer() {
+function Footer() {
   
 
-//   return (
-//     <>
-//       {
-//       /* Navigation bar */
-//       }
-//       <footer>
-//         <hr/>
-//       <h5>&copy; HostelHub,Inc. Terms. Policy</h5>
-//       </footer>
+  return (
+    // <>
+    //   {
+    //   /* Navigation bar */
+    //   }
+    //   <footer>
+    //     <hr/>
+    //   <h5>&copy; HostelHub,Inc. Terms. Policy</h5>
+    //   </footer>
    
-//     </>
-//   );
-// }
+    // </>
+  );
+}
 
-// export default Footer;
+export default Footer;
