00483ff797d4176164342256f79ccb5543f72795
MuziraS <stakaudens@gmail.com>
2023-10-16 09:59:49 +0300

    footer start

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+
+
+import React from "react";
+import "./style.css";
+
+// function Footer() {
+  
+
+//   return (
+//     <>
+//       {
+//       /* Navigation bar */
+//       }
+//       <footer>
+//         <hr/>
+//       <h5>&copy; HostelHub,Inc. Terms. Policy</h5>
+//       </footer>
+   
+//     </>
+//   );
+// }
+
+// export default Footer;
