05fdd1079aa47132a0b6489feb4c0f37215b4dae
joshua-pirwoth <joshuapirwoth06@gmail.com>
2023-10-17 13:55:04 +0300

    Carousel and card components; The card contains the carousel

--- src/HostelCard.js
+++ src/HostelCard.js
@@ -2,33 +2,36 @@ import React from 'react';
 import './card.css';
 import HostelCarousel from './HostelCarousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 const HostelCard = () => {
+    
     const hostelName = "Hostel Name";
     const hostelLocation = "Location (e.g. Kikoni, Makerere)";
     const availableRooms = 120;
-    const ratingIcon = "./assets/rating_icon.svg";
+    /* const ratingIcon = "./assets/rating_icon.svg"; */
     const rating = 4.5;
 
     return ( 
-        
-        <div className="hostel-card">
-            {/* Carousel */}
-            <HostelCarousel />
-            {/* Hostel details */}
-            <div className="hostel-details">
-                <div className='details-header'>
-                <h3>{ hostelName }</h3>
-                <div className='rating'>
-                    <img src={ ratingIcon } alt="rating_icon" />
-                    <p>{ rating }</p>
-                </div>
-                </div>
-                
-                <p>{ hostelLocation }</p>
-                <p>{ availableRooms } rooms available</p>
-            </div>
-        </div>
+    <div className="conatiner">
+      <Card>
+      {/* Carousel */}
+      <HostelCarousel />
+      <Card.Body>
+        <Card.Title>
+          { hostelName }
+          {/* <div className='hostel-rating d-flex flex-column justify-content-space-between align-items-center' style={{width: '15%'}}>
+              <img src={ ratingIcon } alt="rating_icon" /> */}
+          { rating }
+            {/* </div> */} 
+        </Card.Title>
+        <Card.Text>
+            <p>{ hostelLocation }</p>
+            <p>{ availableRooms } rooms available</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
      );
 }
  
