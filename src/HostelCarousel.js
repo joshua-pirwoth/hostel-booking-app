fdc3e23af8f22708eafb31175358f5bed452132f
joshua-pirwoth <joshuapirwoth06@gmail.com>
2023-10-18 22:36:08 +0300

    Edited the Carousel to implement genuine CSS

--- src/HostelCarousel.js
+++ src/HostelCarousel.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import image1 from'./assets/image1.jpg';
 import image2 from'./assets/image2.jpg';
 import image3 from'./assets/image3.jpg';
@@ -11,19 +13,32 @@ function HostelCarousel() {
     setIndex(selectedIndex);
   };
 
-  return (
-    <div className='hostel-carousel'>
-      <Carousel fade>
-        <Carousel.Item>
-        <img className='d-block w-100' src={ image1 } alt="First Slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-        <img className='d-block w-100' src={ image2 } alt="Second Slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-        <img className='d-block w-100' src={ image3 } alt="Third Slide" />
-        </Carousel.Item>
-      </Carousel>
+  return (    
+    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1" className="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2" className="active"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={ image1 } className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={ image2 } className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={ image3 } className="d-block w-100" alt="..."/>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        {/* <span className="sr-only">Previous</span> */}
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        {/* <span className="sr-only">Next</span> */}
+      </button>
     </div>
   );
 }
