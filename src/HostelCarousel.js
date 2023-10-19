94e09a3df32e64bd90e66e4a7fc7283e796266f4
joshua-pirwoth <joshuapirwoth06@gmail.com>
2023-10-19 12:55:43 +0300

    Updated carousel indicators for the Carousel Component

--- src/HostelCarousel.js
+++ src/HostelCarousel.js
@@ -15,11 +15,11 @@ function HostelCarousel() {
 
   return (    
     <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
+        {/* <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
+        </ol> */}
       <div className="carousel-inner">
           <div className="carousel-item active">
               <img src={ image1 } className="d-block w-100" alt="..."/>
