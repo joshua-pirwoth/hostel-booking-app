fc7d7283d550bf90c95f75ecaaed0966bfa161be
joshua-pirwoth <joshuapirwoth06@gmail.com>
2023-10-19 19:31:18 +0300

    Corrcted indentation in  hostel card

--- src/HostelCard.js
+++ src/HostelCard.js
@@ -9,20 +9,20 @@ const HostelCard = () => {
   const col1_styles = { maxWidth: '288px', maxHeight: '288px'};
   const col2_styles = { height: '86px' };
 
-    return (
-      <div className="m-3 col-md-3 position-relative" style={ containerStyles }>
-        <div className='row'>
-            <div className='col w-100 h-100 position-absolute p-0 overflow-hidden' style={ col1_styles }>
-              {/* Carousel */}
-              <HostelCarousel />
-            </div>
+  return (
+    <div className="m-3 col-md-3 position-relative" style={ containerStyles }>
+      <div className='row'>
+        <div className='col w-100 h-100 position-absolute p-0 overflow-hidden' style={ col1_styles }>
+          {/* Carousel */}
+          <HostelCarousel />
         </div>
-        <div className='row'>
-            <div className='col w-100 position-absolute p-0 bottom-0' style={ col2_styles }>
-              {/* Details */}
-              <HostelDetails />
-            </div>
+      </div>
+      <div className='row'>
+        <div className='col w-100 position-absolute p-0 bottom-0' style={ col2_styles }>
+          {/* Details */}
+          <HostelDetails />
         </div>
+      </div>
     </div>
      );
 }
