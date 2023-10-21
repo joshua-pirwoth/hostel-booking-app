bb305b38563cd683fbf4edf60a0e44dbd0697bef
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2023-10-21 21:38:18 +0300

    Reset App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import Home from './Home';
-
-function App() {
-    return (
-    <div className="App">
-      {/* Navbar */}
-      {/* Home */}
-      <Home />
-      {/* Footer */}
-    </div>
-  );
+const App = () => {
+  return ( 
+    <>
+      
+    </>
+   );
 }
-
+ 
 export default App;
