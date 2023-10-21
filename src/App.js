00b2c47f1a0dc113e366c6e4699e1867c6588bc5
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2023-10-21 21:50:37 +0300

    Added updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-const App = () => {
-  return ( 
-    <>
-      
-    </>
-   );
+import CarouselContainer from "./components/carouselContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselContainer />
+    </div>
+  );
 }
- 
+
 export default App;
