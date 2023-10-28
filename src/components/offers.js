7cb701088c11ebc17a9ba7d5577dd13fea34225c
Muzira Stuart <127627733+MuziraS@users.noreply.github.com>
2023-10-28 11:16:20 +0300

    offers component part of the details page

---
+++ src/components/offers.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css'
+
+function Offers (){
+    return(
+        <>
+        <p2>What this hostel offers:</p2>
+        <div className='list'>
+        <ul >
+            <li>Free DSTv</li>
+            <li>24/7 WiFi</li>
+            <li>Pool table</li>
+            <li>Hostel Shuttle</li>
+
+        </ul>
+      
+        </div>
+       
+        
+        
+        </>
+    )
+}
+
+export default Offers;
