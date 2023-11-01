1dc162df83431f52756a14f2e155e0224e7682c5
Nanziri Jovia <joviananziri813@gmail.com>
2023-11-01 09:59:23 -0700

    intial commit for login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,51 @@
-import './Register.css';
+import google from "../assets/search.png"
+
 
 const login = () => {
     return ( 
-        <div className='d-flex justify-content-center align-items-center bg-white w-100'>
-        <div className ='bg-white p-3 rounded w-25 shadow'>
-            <form>
+        <div className='container'>
+            <div className='row justify-content-center px-5  d-flex vh-100 align-items-center'>
+                <div className='col-sm-8 col-md-7 col-lg-4 pb-3 rounded  shadow'>
+                   <form>
             
-                <div>
-                    <div className= "text-center">Log in</div>
-                    <div className="underline"></div>
-                </div>
+                        <div>
+                            <div className= "text-center pt-2 fs-5 fw-bold">Log in</div>
+                            <hr />
+                        </div>
             
-                <div>
-                    <h5>Welcome to HostelHub</h5>
-                </div>
-                <div className='mb-3'>
-                   <input type ="email" placeholder ="email" className="form-control" name="email"/>
-                </div>
-                <div className="mb-3">
-                   <input type ="password" placeholder ="password" className="form-control" name="password"/>
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-primary w-100">Continue</button>
-                </div>    
-                <div className= "text-center">
-                   <p>or</p>
-                </div>
-                <div>
-                    <button className="btn-btn-default border w-100 bg-light rounded">Sign in with Google Account</button>
-                
-                </div>
+                        <div>
+                            <h5 className='ps-2 py-1'>Welcome to HostelHub</h5>
+                        </div> 
+                        <div className='mb-3'>
+                            <input type ="email" placeholder ="email" className="form-control" name="email"/>
+                        </div>
+                        <div className="mb-3">
+                            <input type ="password" placeholder ="password" className="form-control" name="password"/>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary w-100">Continue</button>
+                        </div>    
+                        <div className='container'>
+                            <div className= "row">
+                            <div className='col-5'><hr/></div>
+                            <p className='px-2 col-1'>or</p>
+                            <div className='col-5'><hr/></div>
 
-            </form>
+                        </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-outline-light text-secondary border w-100 rounded" style={{width:"100px"}}><div className='d-flex ps-2 pt-1 align-items-center'> <img className='img-fluid' style={{width:"20px", height:"20px"}} src={google} alt="google"/><span className='flex-grow-1'>Continue with Google </span></div></button>
+                
+                        </div>
+
+                     </form>
+                </div>
+            </div>
         </div>
-    </div>
      
     
 
-     );
+    );
 }
  
 export default login;
