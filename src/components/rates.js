6129324bc5aad427f04e975e3f03d1f0ecbc9891
Muzira Stuart <127627733+MuziraS@users.noreply.github.com>
2023-11-01 14:27:19 +0300

    refine rates component

--- src/components/rates.js
+++ src/components/rates.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import './style.css'
 
 import Button from 'react-bootstrap/Button';
 
 function Rates() {
     return (
-      
-            <div className="rate-container">
-              <h2>Rates</h2>
-              <hr/>
-              <table>
-                <tr>
-                    <td className="px-4">Single Room <br/>UGX..............</td>
-                  
-                    <td className='px-4'><Button variant="primary" type="submit" className="btn">Book</Button></td>
+      <div className="bg-white shadow p-4 rounded-2xl">
 
-                </tr>
-              </table>
-              <hr/>
-              <table>
-                <tr>
-                    <td className="px-4">Single Room <br/>UGX..............</td>
-                  
-                    <td className='px-4'><Button variant="primary" type="submit" className="btn">Book</Button></td>
-
-                </tr>
-              </table>
-            </div>
-          
+      </div>
+            
         
       
     );
