5dd6d29ef8377344037bc45795761cefda47d527
MuziraS <stakaudens@gmail.com>
2023-11-10 12:25:18 +0300

    navbar and offers

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,13 @@ import React from "react";
 import { Link} from "react-router-dom";
 import "./style.css";
 
+
+
 // import Button from "./button"; // Import the Button component
 // import Dropdown from "./dropdown"; // Import the Dropdown component
 
 function Navbar() {
+
   // State to control the visibility of the dropdown menu
   // const [dropdown, setDropdown] = useState(false);
 
@@ -29,8 +32,13 @@ function Navbar() {
         <div className="search-item"></div>
         <input type="text" class="search-input" placeholder="   Search.....  "></input>
       </div>
-      <div className="profile-container flex">
-
+      <div className="">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>
       </div>
             
         <hr/>
--- src/components/offers.js
+++ src/components/offers.js
@@ -34,7 +34,12 @@ function HostelList(){
         </div>
         
       </div>
-      <button className='border p-2 flex rounded-2 cursor-pointer rounded-full py-2 px-4'>Show more amenities</button>
+      <button className='border p-2 flex rounded-2 cursor-pointer rounded-full py-2 px-4'>
+     Show more amenities   
+     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+</svg></button>
+  
     </div>
 
     
