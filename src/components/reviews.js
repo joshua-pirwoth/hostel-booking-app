2d2f3958669ffc2f3d5ffe99a2e9d6a3d1e2485e
MuziraS <stakaudens@gmail.com>
2023-11-14 18:03:25 +0300

    reviews done

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -13,6 +13,12 @@ function ReviewsList() {
           <ul className="list-unstyled list-group-custom">
             {/* Reviews items go here */}
             <li className='list-custom'>
+            <div>
+    <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>John Doe. </b>     <i className="text-muted">Jinja, Uganda</i>       
+    </p>
+</div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
@@ -29,6 +35,10 @@ function ReviewsList() {
 
             </li>
             <li className='list-custom'>
+            <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>Odongo Joshua. </b>     <i className="text-muted">Kampala, Uganda</i>       
+    </p>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
@@ -42,6 +52,10 @@ function ReviewsList() {
  <p>The security at the student hostel was top-notch, with electronic key systems and good-sized lockers. The staff was also very pleasant and accommodating, making for a great stay in Montevideo.</p>
               </li>
               <li className='list-custom'>
+              <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>Joan Ahereza. </b>     <i className="text-muted">Mbarara, Uganda</i>       
+    </p>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
@@ -66,6 +80,10 @@ function ReviewsList() {
           <ul className="list-unstyled list-group-custom">
             {/* Reviews items go here */}
             <li className='list-custom'>
+            <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>Nabukera Joan. </b>     <i className="text-muted">Mbale, Uganda</i>       
+    </p>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
@@ -84,6 +102,10 @@ function ReviewsList() {
  <p>The kitchen at the student hostel was well-equipped and clean, and the free coffee was a nice touch. The other guests were friendly and respectful, making for a pleasant stay</p>
               </li>
               <li className='list-custom'>
+              <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>Janet Nalubowa. </b>     <i className="text-muted">Mukono, Uganda</i>       
+    </p>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
@@ -102,6 +124,10 @@ function ReviewsList() {
  <p>The kitchen at the student hostel was well-equipped and clean, and the free coffee was a nice touch. The other guests were friendly and respectful, making for a pleasant stay</p>
               </li>
               <li className='list-custom'>
+              <p className="float-left"><img src={require('./images/ron.jpg')} height="60px" width="60px" style={{ border: '1px solid black' }} className="rounded-circle" alt="Ron" />
+    &nbsp;  &nbsp;    &nbsp;  &nbsp;
+     <b>Muwanika Moses. </b>     <i className="text-muted">Kampala, Uganda</i>       
+    </p>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
  </svg>  &nbsp;
